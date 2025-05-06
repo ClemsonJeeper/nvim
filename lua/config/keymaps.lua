24ae97fe65c132149443c745002ebf82fed33764
Rob Johnston <clemsonjeeper@gmail.com>
2025-05-06 19:17:54 +0000

    Remove redundant plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,32 +10,38 @@ mapkey("<leader>/", "nohlsearch", "n")
 mapkey("_s", "StripWhitespace", "n")
 
 -- Pane and Window Navigation
-mapkey("<C-h>", "<C-w>h", "n") -- Navigate Left
-mapkey("<C-j>", "<C-w>j", "n") -- Navigate Down
-mapkey("<C-k>", "<C-w>k", "n") -- Navigate Up
-mapkey("<C-l>", "<C-w>l", "n") -- Navigate Right
-mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
-mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
-mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
-mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
-mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
-mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
-mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
-mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
-
--- Bind shift-arrows to navigation
-mapkey("<F11>", "<C-w>h", "i") -- Navigate Left
-mapkey("<F10>", "<C-w>j", "i") -- Navigate Down
-mapkey("<F2>", "<C-w>k", "i") -- Navigate Up
-mapkey("<F12>", "<C-w>l", "i") -- Navigate Right
-mapkey("<F11>", "wincmd h", "t") -- Navigate Left
-mapkey("<F10>", "wincmd j", "t") -- Navigate Down
-mapkey("<F2>", "wincmd k", "t") -- Navigate Up
-mapkey("<F12>", "wincmd l", "t") -- Navigate Right
-mapkey("<F11>", "TmuxNavigateLeft", "n") -- Navigate Left
-mapkey("<F10>", "TmuxNavigateDown", "n") -- Navigate Down
-mapkey("<F2>", "TmuxNavigateUp", "n") -- Navigate Up
-mapkey("<F12>", "TmuxNavigateRight", "n") -- Navigate Right
+-- mapkey("<C-h>", "<C-w>h", "n") -- Navigate Left
+-- mapkey("<C-j>", "<C-w>j", "n") -- Navigate Down
+-- mapkey("<C-k>", "<C-w>k", "n") -- Navigate Up
+-- mapkey("<C-l>", "<C-w>l", "n") -- Navigate Right
+-- mapkey("<C-h>", "wincmd h", "t") -- Navigate Left
+-- mapkey("<C-j>", "wincmd j", "t") -- Navigate Down
+-- mapkey("<C-k>", "wincmd k", "t") -- Navigate Up
+-- mapkey("<C-l>", "wincmd l", "t") -- Navigate Right
+-- mapkey("<C-h>", "TmuxNavigateLeft", "n") -- Navigate Left
+-- mapkey("<C-j>", "TmuxNavigateDown", "n") -- Navigate Down
+-- mapkey("<C-k>", "TmuxNavigateUp", "n") -- Navigate Up
+-- mapkey("<C-l>", "TmuxNavigateRight", "n") -- Navigate Right
+--
+-- -- Bind shift-arrows to navigation
+-- mapkey("<S-Left>", "wincmd h", "n") -- Navigate Left
+-- mapkey("<S-Down>", "wincmd j", "n") -- Navigate Down
+-- mapkey("<S-Up>", "wincmd k", "n") -- Navigate Up
+-- mapkey("<S-Right>", "wincmd l", "n") -- Navigate Right
+--
+-- -- Bind shift-arrows to navigation
+-- mapkey("<F11>", "<C-w>h", "i") -- Navigate Left
+-- mapkey("<F10>", "<C-w>j", "i") -- Navigate Down
+-- mapkey("<F2>", "<C-w>k", "i") -- Navigate Up
+-- mapkey("<F12>", "<C-w>l", "i") -- Navigate Right
+-- mapkey("<F11>", "wincmd h", "t") -- Navigate Left
+-- mapkey("<F10>", "wincmd j", "t") -- Navigate Down
+-- mapkey("<F2>", "wincmd k", "t") -- Navigate Up
+-- mapkey("<F12>", "wincmd l", "t") -- Navigate Right
+-- mapkey("<F11>", "TmuxNavigateLeft", "n") -- Navigate Left
+-- mapkey("<F10>", "TmuxNavigateDown", "n") -- Navigate Down
+-- mapkey("<F2>", "TmuxNavigateUp", "n") -- Navigate Up
+-- mapkey("<F12>", "TmuxNavigateRight", "n") -- Navigate Right
 
 -- Window Management
 mapkey("<leader>|", "vsplit", "n", { desc = "Split window vertically" })
